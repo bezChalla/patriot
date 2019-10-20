cec29e39c19dcd05aead8dae356e6edb63422ce2
rtacaray <rtacaray@masonlive.gmu.edu>
2019-10-20 11:57:53 -0400

    Something is Wrong

--- script.js
+++ script.js
@@ -24,26 +24,26 @@ $(document).ready(function(){
             let tempInput = document.getElementById("temp").nodeValue;
             
             //set variables for calculating each component score
-            let wasteScore = "";
-            let transScore = "";
-            let waterScore = "";
-            let energyScore = "";
-            let totalTransScore = "";
-            let totalWaterScore = "";
-            let totalTempScore = "";
+            let wasteScore = 0;
+            let transScore = 0;
+            let waterScore = 0;
+            let energyScore = 0;
+            let totalTransScore = 0;
+            let totalWaterScore = 0;
+            let totalTempScore = 0;
 
-            let totalScore = "";
+            let totalScore = 0;
 
             if (document.getElementById("optionsRadio1").checked) {
 				wasteScore = 0;
 			} else if (document.getElementById("optionsRadio2").checked) {
-				wasteScore = 184;
+				wasteScore = 0;
 			} else if (document.getElementById("optionsRadio3").checked) {
-				wasteScore = 184;
+				wasteScore = 0;
 			}else if (document.getElementById("optionsRadio4").checked) {
-				wasteScore = 184;
-			}else {
-				wasteScore = 184;
+				wasteScore = 0;
+			}else if (document.getElementById("optionsRadio5").checked) {
+				wasteScore = 0;
             }
             
             if (document.getElementById("optionsRadio6").checked) {
@@ -54,13 +54,13 @@ $(document).ready(function(){
 				transScore = 22;
 			}else if (document.getElementById("optionsRadio9").checked) {
 				transScore = 200;
-			}else {
+			}else if(document.getElementById("optionsRadio10").checked){
 				transScore = 100;
             }
             
             if (document.getElementById("optionsRadio11").checked) {
 				waterScore = 204;
-			}else {
+			}else if(document.getElementById("optionsRadio12").checked){
 				waterScore = 0;
             }
             
@@ -87,8 +87,8 @@ $(document).ready(function(){
             //console.log("Aluminum and tin score is: " + alumTinScore);
 
 			if (milesInput === 0 || milesInput === "undefined") {
-				toatalTransScore = 0;
-			} else {
+				totalTransScore = 0;
+			} else if (milesInput > 0) {
 				totalTransScore = milesInput * transScore;
 			}
 			// console.log("Electric score is: " + electricScore);
@@ -96,22 +96,22 @@ $(document).ready(function(){
 			if (showMinInput === 0 || showMinInput === "undefined") {
 				totalWaterScore = 0;
 			} else {
-				totalWaterScore = showMinInput * 204;
+				totalWaterScore = showMinInput * waterScore;
 			}
 			// console.log("Gas score is: " + gasScore);
 
 			if (tempInput === 0 || tempInput === "undefined") {
 				totalTempScore = 0;
-			} else {
+			} else if (tempInput > 0) {
 				totalTempScore = (72-tempInput) * 54431;
             }
             
             
 
              //diet
-            let dietPrint=""; //food eaten - food eaten at Ikes 
+            let dietPrint=0; //food eaten - food eaten at Ikes 
 
-            let foodCunter="";// footprint per mean in pounds 
+            let foodCunter=0;// footprint per mean in pounds 
             if(document.getElementById("beef").checked){
                 foodCunter =+ 6.1;
             }if(document.getElementById("cheese").checked){
@@ -128,7 +128,7 @@ $(document).ready(function(){
                 foodCunter += .16;
             }if(document.getElementById("legumes").checked){
                 foodCunter += .11;
-            }if(document.getElementById("carrosts").checked){
+            }if(document.getElementById("carrots").checked){
                 foodCunter += .07;
             }if(document.getElementById("potatos").checked){
                 foodCunter += .03;
@@ -145,8 +145,19 @@ $(document).ready(function(){
 
             //recycling
 
+            let reducing=0;// footprint reduced in pounds
+            if(document.getElementById("can").checked){
+                reducing += 89;
+            }if(document.getElementById("paper").checked){
+                reducing += 113;
+            }if(document.getElementById("plastic").checked){
+                reducing += 36;
+            }if(document.getElementById("glass").checked){
+                reducing += 25;
+            }
+
             // calculate total score and round to nearest whole integer
-			totalScore = Math.round(totalWaterScore + totalTransScore + totalTempScore + energyScore);
+			totalScore = Math.round(totalWaterScore + totalTransScore + totalTempScore + energyScore + dietPrint - reducing);
 			let formattedScore = totalScore.toLocaleString("en");
             // console.log(totalScore);
 
